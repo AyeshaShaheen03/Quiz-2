ec4e77d65c930020971276e7423e8f1d9b408a02
Ayesha Shaheen <ayeshashaheen542@gmail.com>
2021-12-04 20:38:17 +0500

    second quiz

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           tutorial Object1 = new tutorial();
+           Object1.tutorialID= 1;
+           Object1.tutorialName= "c#";
+           Object1.introduce();
+           tutorial Object2 = new tutorial();
+           Object2.tutorialID= 2;
+           Object2.tutorialName= "ASP.Net";
+           Object2.introduce();
+           tutorial Object3 = new tutorial ();
+           Object3.tutorialID= 3;
+           Object3.tutorialName= "VB.Net";
+           Object3.introduce();
+          
+
+
+        }
+    }
+}
---
+++ tutorial.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Quiz
+{
+    public class tutorial
+
+    {
+      public float tutorialID;
+      public string tutorialName;
+      public void introduce()
+      {
+      Console.WriteLine("TutorialName =" + tutorialName + "."+"\n Tutorial ID=" + tutorialID);
+      }
+
+        
+    }
+}
